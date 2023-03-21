5e187a911f8f64994a863758636912e265f28892
Sonianupur <nupur9808012@yahoo.co.uk>
2023-03-21 11:29:08 +0000

    W5 seminer

--- demos/W5_tinder.py
+++ demos/W5_tinder.py
@@ -1,1 +1,25 @@
-print("hahahahhhaahhahaahahaha")
+def interests():
+    print("Enter all your interest, one after another or \"stop\" ")
+    s1 = set()
+    interest = ""
+    while interest != "stop":
+        interest = input()
+        if interest.lower() == "stop":
+            break
+        s1.add(interest)
+    return s1
+def tinderDasecond():
+    print("first person: ")
+    p1 = interests()
+    print("second person: ")
+    p2 = interests()
+    common = p1.intersection(p2)
+    if len(common)>= 3:
+        print(f"You are a match made in heaven! You have {len(common)} interests in common")
+    else:
+        print("oh no. its not gonna work. find another person.")
+tinderDasecond()
+
+
+
+#print(interests())
--- demos/w5_dicts.py
+++ demos/w5_dicts.py
@@ -1,4 +1,28 @@
+#initialize empty dictionary
+d  = {}
 dictio = dict()
 # initialise non-empty dictionary
-phonebook = { " Thomas": 088877766, "aga": 09988766, "mD":2334455}
+phonebook = {"Thomas":7964536124,"Aga":7769988766,"mD":7754234455}
+
 print(phonebook)
+#print/ access of middle element
+name = input("who you gonna call: ")
+if name in phonebook:
+
+    print(f"callin....{phonebook[name]}")
+else:
+    print(f"No number for {name}")
+#zipping two lists together to compose a dictionary
+names = ["garry", "Ian", "Laura"]
+age = [56, 21, 16]
+people = dict(zip(names, age))
+print(people)
+#Traversing dictionary= accessing keys/values/both
+for thing in people:
+    print(thing)
+for thing in people.values():
+    print(thing)
+for thing in people.keys():
+    print(thing)
+for k, v in people.items():
+    print(f"Person {k} is {v} years old")
